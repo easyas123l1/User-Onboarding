0f33fc82a00d4273b74f6ecd5aa237f274bef2b6
Andrew Wilson <andrew.wilson90@hotmail.com>
2019-10-17 14:37:53 -0500

    initial commit

---
+++ onboarding/src/App.js
@@ -0,0 +1,13 @@
+import React from 'react';
+import './App.css';
+import CreateForm from './Components/CreateForm';
+
+function App() {
+  return (
+    <div className="App">
+      <CreateForm />
+    </div>
+  );
+}
+
+export default App;
