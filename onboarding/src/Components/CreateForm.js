7f5c6aa553fbde4cf1bbf169ff1fcfdfb2d081a2
Andrew Wilson <andrew.wilson90@hotmail.com>
2019-10-17 14:46:48 -0500

    create formik form

--- onboarding/src/Components/CreateForm.js
+++ onboarding/src/Components/CreateForm.js
@@ -1,7 +1,36 @@
-import React from 'react';
+import React, {useState, useEffect} from "react";
+import axios from "axios";
+import { withFormik, Form, Field } from "formik";
+import * as Yup from "yup";
 
 const CreateForm = () => {
 
+  return (
+    <div>
+      <Form>
+        <Field 
+        type='text'
+        name='name'
+        placeholder='name' 
+        />
+        <Field
+        type='email'
+        name='email'
+        placeholder='email'
+        />
+        <Field
+        type='password'
+        name='password'
+        placeholder='password'
+        />
+        <Field
+        type='checkbox'
+        name='terms'
+        />
+        <button type='submit'>Submit</button>
+      </Form>
+    </div>
+  )
 }
 
 export default CreateForm;
